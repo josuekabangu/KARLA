e5bc8f45b25dbe0534e799062e0c0507ce806458
josuekabangu <ajkabs2@gmail.com>
2024-06-04 23:42:28 +0200

    Shop models

---
+++ shop/models.py
@@ -0,0 +1,86 @@
+from django.db import models
+from accounts.models import CustomUser
+from django.utils import timezone
+from decimal import Decimal
+
+# Create your models here.
+
+# Modèle pour les catégories de produits
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.name
+    
+# Modèle pour les produits
+class Product(models.Model):
+    category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    digital = models.BooleanField(default=False)
+    image = models.ImageField(upload_to='products/', blank=True)
+    date_added = models.DateTimeField(default=timezone.now)
+    stock = models.PositiveIntegerField()
+    available = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        ordering = ['-date_added']
+
+    @property
+    def image_url(self):
+        if self.image and hasattr(self.image, 'url'):
+            return self.image.url
+        return ''
+    
+# Modèle panier
+class Cart(models.Model):
+    client = models.ForeignKey(CustomUser, on_delete=models.SET_NULL, blank=True, null=True)
+    date_commande = models.DateTimeField(auto_now_add=True)
+    complete = models.BooleanField(default=False, null=True, blank=True)
+    transaction_id = models.CharField(max_length=200, null=True)
+    status = models.CharField(max_length=200, null=True, blank=True)
+    total_trans = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
+
+    def __str__(self):
+        return str(self.id)
+    
+    @property
+    def get_cart_total(self):
+        cartitems = self.cartitem_set.all()
+        total = sum([item.get_total for item in cartitems])
+        return total
+
+    @property
+    def get_cart_items(self):
+        cartitems = self.cartitem_set.all()
+        total = sum([item.quantity for item in cartitems])
+        return total
+
+class CartItem(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.SET_NULL, blank=True, null=True)
+    cart = models.ForeignKey(Cart, on_delete=models.SET_NULL, blank=True, null=True)
+    quantity = models.IntegerField(default=0, null=True, blank=True)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    @property
+    def get_total(self):
+        total = self.product.price * self.quantity
+        return total
+
+class AddressShipping(models.Model):
+    client = models.ForeignKey(CustomUser, on_delete=models.SET_NULL, blank=True, null=True)
+    cart = models.ForeignKey(Cart, on_delete=models.SET_NULL, blank=True, null=True)
+    address = models.CharField(max_length=100, null=True)
+    city = models.CharField(max_length=100, null=True)
+    zipcode = models.CharField(max_length=100, null=True)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.address
+    
+    
